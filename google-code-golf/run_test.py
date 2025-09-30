50d3091f447efd2aec7e4af548acec5943fe2cde
ahsan-500mbps <atariq@knox.edu>
2025-09-30 12:15:11 -0500

    update

--- google-code-golf/run_test.py
+++ google-code-golf/run_test.py
@@ -2,7 +2,7 @@
 import code_golf_utils as utils
 
 # --- Manually change this number to test a different task ---
-task_num =  1
+task_num =  5
 
 print(f"--- Testing Task {task_num:03d} ---")
 examples = utils.load_examples(task_num)
