666228b1ef185b49931099f7a4b97e8451a31c85
Suhrid Abrar <suhridabrarbd@outlook.com>
2025-10-05 19:49:34 -0500

    221-230.py no flags

--- google-code-golf/solutions/task221.py
+++ google-code-golf/solutions/task221.py
@@ -1,1 +1,12 @@
 # TODO  # TO BE DONE BY Suhrid Sadman Abrar
+R=range
+def p(g):
+ f=sum(g,[])
+ S=f.count(0)
+ Z=9-S
+ X=[[0]*(S*3) for i in R(S*3)]
+ for i in R(Z):
+  for r in R(3):
+   for c in R(3):
+    if g[r][c]:X[r+(i//S)*3][c+(i%S)*3]=max(f)
+ return X
--- google-code-golf/solutions/task227.py
+++ google-code-golf/solutions/task227.py
@@ -1,1 +1,2 @@
 # TODO  # TO BE DONE BY Suhrid Sadman Abrar
+p=lambda g:[[2*(g[i][j]==0==g[i+4][j])for j in range(4)]for i in range(4)]
