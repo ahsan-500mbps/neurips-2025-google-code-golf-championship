2ac1b07f98dd560aeb9ce6c63d3bb4c248681088
Suhrid Abrar <suhridabrarbd@outlook.com>
2025-10-05 19:40:15 -0500

    211-220.py 218 219 flagged

--- google-code-golf/run_test_advanced.py
+++ google-code-golf/run_test_advanced.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import importlib.util, traceback, json, sys
 
 # --- Change this number to pick a task ---
-task_num = 204
+task_num = 219
 print(f"--- Testing Task {task_num:03d} ---")
 
 ROOT = Path(__file__).resolve().parent  # repo root regardless of CWD
--- google-code-golf/solutions/task215.py
+++ google-code-golf/solutions/task215.py
@@ -1,1 +1,2 @@
 # TODO  # TO BE DONE BY Suhrid Sadman Abrar
+p=lambda j:[[r for j,r in enumerate(j)if sum(r)and j%3==i%3][0]for i in range(len(j))]
