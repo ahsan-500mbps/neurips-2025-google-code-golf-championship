8fb096ec8b8b4a473954656d007ce364037f6498
Suhrid Abrar <suhridabrarbd@outlook.com>
2025-10-07 17:49:39 -0500

    351-360 all clear

--- google-code-golf/solutions/task357.py
+++ google-code-golf/solutions/task357.py
@@ -1,1 +1,9 @@
-# TODO  # TO BE DONE BY Nafis Fuad
+# TODO  # TO BE DONE BY Nafis Fuad / Suhrid Sadman Abrar
+def p(g,R=range,L=len):
+ h,w=L(g),L(g[0])
+ g=[[8 for i in r]for r in g]
+ C=[i for i in range(w)]
+ C+=C[::-1][1:-1]
+ while L(C)<h:C+=C[:]
+ for r in R(h):g[-(r+1)][C[r]]=1
+ return g
