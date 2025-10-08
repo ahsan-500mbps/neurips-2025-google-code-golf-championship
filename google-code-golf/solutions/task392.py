ed617039686e33ac3b5fb5ba65160ede2e131399
Suhrid Abrar <suhridabrarbd@outlook.com>
2025-10-07 20:56:06 -0500

    391-400.py 392 FLagged

--- google-code-golf/solutions/task392.py
+++ google-code-golf/solutions/task392.py
@@ -1,1 +1,8 @@
 # TODO  # TO BE DONE BY Nafis Fuad
+L=len
+R=range
+def p(g):
+ C=max(sum(g,[]))
+ g=[[5 if c==0 else c for c in r] for r in g]
+ return g
+#FLAGGED
--- google-code-golf/solutions/task393.py
+++ google-code-golf/solutions/task393.py
@@ -1,1 +1,2 @@
 # TODO  # TO BE DONE BY Nafis Fuad
+p=lambda j:[[k]for k,_ in __import__('collections').Counter(i for r in j for i in r).most_common(4)[1:]]
