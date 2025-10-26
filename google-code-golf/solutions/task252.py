1b94c0770d4302902c2c814834079ab6082c92bb
Nafis Fuad <134521970+nafis7241@users.noreply.github.com>
2025-10-25 20:38:09 -0500

    251 and 252 solved(golfed)

--- google-code-golf/solutions/task252.py
+++ google-code-golf/solutions/task252.py
@@ -1,1 +1,11 @@
-# TODO  # TO BE DONE BY Enoch & Nafis
+def p(a):
+ h,w=len(a),len(a[0]);b=[r[:]for r in a]
+ for i in range(h):
+  for j in range(w):
+   c=a[i][j]
+   if c and(i<1 or j<1 or a[i-1][j-1]-c):
+    k=0
+    while i+k<h and j+k<w and a[i+k][j+k]==c:
+     if k&1:b[i+k][j+k]=4
+     k+=1
+ return b
