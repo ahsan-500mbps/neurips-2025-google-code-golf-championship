c84df49a0a08985e6981ae5a6820905438974b94
Ahsan Tariq <atariq@knox.edu>
2025-10-26 13:12:55 -0500

    update

--- google-code-golf/solutions/task078.py
+++ google-code-golf/solutions/task078.py
@@ -1,1 +1,4 @@
-#TODO  # TO BE DONE BY AHSAN TARIQ
+def p(j):
+ R=range;a=len(j);b=len(j[0]);k=[[0]*b for _ in R(a)]
+ for x in R(b):v=[j[y][x]for y in R(a)if j[y][x]];[k[i].__setitem__(x,v[i])for i in R(len(v))]
+ return k
