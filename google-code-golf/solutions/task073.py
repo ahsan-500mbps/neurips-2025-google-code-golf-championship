8f35c058fe53347172d63de818c667537f4c7b8c
Ahsan Tariq <atariq@knox.edu>
2025-10-26 12:45:37 -0500

    update

--- google-code-golf/solutions/task073.py
+++ google-code-golf/solutions/task073.py
@@ -1,1 +1,6 @@
-#TODO  # TO BE DONE BY AHSAN TARIQ
+def p(j):
+ a=[r[:]for r in j]
+ for x in range(5):
+  for y in range(5):
+   if j[y][x]==1:a[y][x]=0;a[4][x]=1
+ return a
--- google-code-golf/solutions/task075.py
+++ google-code-golf/solutions/task075.py
@@ -1,1 +1,9 @@
-#TODO  # TO BE DONE BY AHSAN TARIQ
+def p(j):
+ R=range;a=[r[:]for r in j];e=[[j[y][x]for x in R(3)]for y in R(3)]
+ for y in R(9):
+  for x in R(4,13):
+   if j[y][x]==1:
+    for Y in R(-1,2):
+     for X in R(-1,2):
+      if 0<=y+Y<9and 4<=x+X<13:a[y+Y][x+X]=e[Y+1][X+1]
+ return a
