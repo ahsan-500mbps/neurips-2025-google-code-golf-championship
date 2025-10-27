9fe40f456a84bd3fc77f734adce1c6a894e6f0bf
Tioluwani Enoch <156959156+tioluwani-enoch@users.noreply.github.com>
2025-10-27 11:10:10 -0500

    added the corrections to the ones that where flagged as well as shortened some that where longer and ensured that they still worked properly

--- google-code-golf/solutions/task104.py
+++ google-code-golf/solutions/task104.py
@@ -1,6 +1,6 @@
 def p(g):
- e,Z=[],[g[0][0],g[0][2],g[2][2],g[2][0]]
- for r in[(3,0),(0,3)]:[e.append([*sum(([c]*4 for c in r),[]),0])for _ in range(4)]
- e+=[[0]*len(e[0])]
- for _ in range(Z.index(3)):e=[list(r)for r in zip(*e[::-1])]
- return e
+ a=[[3]*4+[0]*5]*4+[[0]*4+[3]*4+[0]]*4+[[0]*9]
+ for k in g[0][0],g[0][2],g[2][2],g[2][0]:
+  if k:break
+  a=[*map(list,zip(*a[::-1]))]
+ return a
