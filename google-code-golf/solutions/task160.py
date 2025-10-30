9c3963b6585d199eafae66bd690d1b875007cde6
Tioluwani Enoch <156959156+tioluwani-enoch@users.noreply.github.com>
2025-10-29 23:32:16 -0500

    added the last changes

--- google-code-golf/solutions/task160.py
+++ google-code-golf/solutions/task160.py
@@ -1,6 +1,11 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-p=lambda g:[[2 if flip(g,1)[y][x]==1 and g[y][x]==0 else g[y][x] for x in range(len(g[0]))]for y in range(len(g))]
-
-#Flagged
+R=range
+L=len
+def p(g):
+ h,w=L(g),L(g[0])
+ for r in R(h-2):
+  for c in R(w-2):
+   if g[r][c]==1 and sum(g[r][c:c+3]+g[r+1][c:c+3]+g[r+2][c:c+3])==8:
+    for y in R(3):
+     for x in R(3):g[r+y][c+x]=2
+     g[r][c]=0;g[r][c+2]=0;g[r+2][c]=0;g[r+2][c+2]=0
+ return g
